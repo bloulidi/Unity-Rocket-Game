8e7a3dd64350e984fe7583a161a20ab1ff6bdebe
Badreddine Loulidi <badred123456@gmail.com>
2021-02-04 18:17:36 -0500

    Added thrusting

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate Left Pressed");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotate Right Pressed");
+        }
     }
 }
