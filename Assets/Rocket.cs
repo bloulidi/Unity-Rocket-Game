ace9ba6cd70de93dc15e6aa69c40cf8f6b53e085
Badreddine Loulidi <badred123456@gmail.com>
2021-02-04 18:35:28 -0500

    Added rotation

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -27,11 +27,11 @@ public class Rocket : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotate Left Pressed");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotate Right Pressed");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
